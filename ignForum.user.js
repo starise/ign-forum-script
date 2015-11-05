13b8c464be543db1b30767ee02115ef9b2bf25b1
Andrea Brandi <starise@gmail.com>
2015-11-05 19:39:46 +0100

    Initial commit

---
+++ ignForum.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         IGN Forum Background Change
+// @namespace    https://github.com/starise/ign-forum-script
+// @version      0.1.0
+// @author       starise
+// @match        *://it.ign.com/forum/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/starise/ign-forum-script/master/ignForum.user.js
+// @updateURL    https://raw.githubusercontent.com/starise/ign-forum-script/master/ignForum.user.js
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+document.body.style.background = "#000";
